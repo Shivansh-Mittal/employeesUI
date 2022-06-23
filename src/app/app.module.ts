e14e975a60f96f2716d483aaecfb32af62dc7c5a
Shivansh Mittal <81565355+Shivansh-Mittal@users.noreply.github.com>
2022-06-23 20:33:46 +0530

    Add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,40 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { DialogModule } from 'primeng/dialog';
+import {MenubarModule} from 'primeng/menubar';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { EmployeesListComponent } from './employees-list/employees-list.component';
+import { AditEmployeeComponent } from './adit-employee/adit-employee.component';
+import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { MsgService } from './shared/services/msg.service';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    EmployeesListComponent,
+    AditEmployeeComponent,
+    HomeComponent,
+    PageNotFoundComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    DialogModule,
+    MenubarModule,
+    FormsModule
+  ],
+  providers: [MsgService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
